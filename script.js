89e43e7b6b3b394dc5d30401c3188dd15bb68690
rheamordani <rhea@mordani.com>
2022-02-19 11:57:09 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function houses(){
+  document.write('<iframe src=houses-form.html>');
+}
+function stocks(){
+  document.write('<iframe src=stocks-form.html>');
+}
+function donations(){
+  document.write('<iframe src=donations-form.html>');
+}
